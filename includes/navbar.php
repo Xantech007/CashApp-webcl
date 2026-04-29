9a9099d8fb7446099f0df5e11c2a8a8255ac8a40
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-04-29 08:47:25 +0100

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,72 +1,73 @@
-<div class="mainmenu-area"style="background:linear-gradient(to bottom,#C4C4C3,#F5F5F5);border-bottom:none">
-		<div class="container">
-			<div class="row">
-				
-				<div class="col-lg-12">                 
-					<nav class="navbar navbar-expand-lg navbar-light">
-						<?php
-						$logo = "SELECT logo FROM settings";
-						$logo_query = mysqli_query($con ,$logo);
+<div class="mainmenu-area" style="background:linear-gradient(to bottom,#C4C4C3,#F5F5F5); border-bottom:none">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    
+                    <?php
+                    $logo = "SELECT logo FROM settings";
+                    $logo_query = mysqli_query($con ,$logo);
+                    if($logo_query) {
+                        $row = mysqli_fetch_array($logo_query);
+                        $logo_image = $row['logo'];
+                    }
+                    ?>
+                    
+                    <a class="navbar-brand" href="index" style="display:flex;align-items:center;justify-content:center;flex-direction:column">
+                        <img src="uploads/logo/<?= $logo_image ?>" alt="">
+                    </a>
 
-						if($logo_query)
-						{
-							$row = mysqli_fetch_array($logo_query);
-							$logo_image = $row['logo'];					
-						}
-						?>
-						<a class="navbar-brand" href="index"style="display:flex;align-items:center;justify-content:center;flex-direction:column">
-							<img src="uploads/logo/<?= $logo_image  ?>" alt="">							
-						</a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_menu" aria-controls="main_menu"
-							aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						<div class="collapse navbar-collapse fixed-height" id="main_menu">
-							<ul class="navbar-nav ml-auto">
-								<li class="nav-item">
-									<a class="nav-link " href="index">Home
-											<div class="mr-hover-effect"></div></a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="about">About
-											<div class="mr-hover-effect"></div></a>
-								</li>
-								
-								<li class="nav-item">
-									<a class="nav-link " href="faq">FAQ
-											<div class="mr-hover-effect"></div></a>
-								</li>
-								
-							
-								<li class="nav-item">
-									<a class="nav-link " href="contact">Contact
-											<div class="mr-hover-effect"></div></a>
-								</li>
-							</ul>
-							<?php
-							if(isset($_SESSION['admin']))
-							{ ?>
-								<a href="admin/signin" class="base-btn2"style="background: linear-gradient(to bottom, #f7941d, #f76b1c);"> Admin</a>							
-						  <?php	}						
-						
-							else if(isset($_SESSION['auth']))
-							{ ?>
-								<a href="signin" class="base-btn2"style="background: linear-gradient(to bottom, #f7941d, #f76b1c);"> Dashboard</a>							
-						  <?php	}
-						  else
-						  { ?>
-						  <a href="signin" class="base-btn2"style="background: linear-gradient(to bottom, #f7941d, #f76b1c);"> Login</a>
-						 <?php }
-							?>
-							
-							
-						
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!--Main-Menu Area Start-->
-	<div style="margin-top:60px;position:fixed;top:0px;width:100%;z-index:500">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_menu" 
+                            aria-controls="main_menu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse fixed-height" id="main_menu">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index">Home
+                                    <div class="mr-hover-effect"></div>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about">About
+                                    <div class="mr-hover-effect"></div>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="faq">FAQ
+                                    <div class="mr-hover-effect"></div>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact">Contact
+                                    <div class="mr-hover-effect"></div>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <!-- Language Translator -->
+                        <div class="d-flex align-items-center ms-3">
+                            <?php include('includes/translate.php'); ?>
+                        </div>
+
+                        <!-- Login / Dashboard Button -->
+                        <?php
+                        if(isset($_SESSION['admin'])) { ?>
+                            <a href="admin/signin" class="base-btn2 ms-3" 
+                               style="background: linear-gradient(to bottom, #f7941d, #f76b1c);">Admin</a>
+                        <?php } 
+                        else if(isset($_SESSION['auth'])) { ?>
+                            <a href="signin" class="base-btn2 ms-3" 
+                               style="background: linear-gradient(to bottom, #f7941d, #f76b1c);">Dashboard</a>
+                        <?php } 
+                        else { ?>
+                            <a href="signin" class="base-btn2 ms-3" 
+                               style="background: linear-gradient(to bottom, #f7941d, #f76b1c);">Login</a>
+                        <?php } ?>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
 </div>
